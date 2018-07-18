59aa12a3e55399144fa3fecb0c2bbca201e8c4e3
daidalong <daidalong@foxmail.com>
2018-07-18 22:44:24 +0800

    修改js

---
+++ src/main/java/com/ddl/core/utils/SqrtUtil.java
@@ -0,0 +1,41 @@
+package com.ddl.core.utils;
+
+public class SqrtUtil {
+
+	public static double sqrt(double c) {
+		if (c < 0)
+			return Double.NaN;
+		double err = 1e-15;
+		double t = c;
+
+		while (Math.abs(t - c / t) > err * t)
+			t = (c / t + t) / 2.0;
+
+		return t;
+	}
+
+	public static double kf(double a, int n) {
+		if (n < 0)
+			return Double.NaN;
+
+		if (n == 1)
+			return a;
+
+		double x1 = (1 + a) / 2.0d;
+
+		for (int j = 0; j < 16; j++) {
+
+			double xn = x1;
+			for (int i = 1; i < n; i++)
+				xn *= x1;
+
+			double xn_1 = x1;
+			for (int i = 1; i < n - 1; i++)
+				xn_1 *= x1;
+
+			xn_1 *= 2.0;
+			x1 = (xn + a) / xn_1;
+		}
+		return x1;
+	}
+}
